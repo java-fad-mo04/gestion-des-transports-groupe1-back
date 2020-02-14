99bd9c5115208b74c59ffc55a13260a45525354e
Audrey Martin <audrey.martin26@gmail.com>
2020-02-14 14:30:43 +0100

    ajout collegue 3

--- src/main/java/dev/StartupListener.java
+++ src/main/java/dev/StartupListener.java
@@ -64,6 +64,7 @@ public class StartupListener {
 		col3.setMotDePasse(passwordEncoder.encode("superpass"));
 		col3.setRoles(Arrays.asList(new RoleCollegue(col3, Role.ROLE_UTILISATEUR),
 				new RoleCollegue(col3, Role.ROLE_CHAUFFEUR)));
+		this.collegueRepo.save(col3);
 
 	}
 
