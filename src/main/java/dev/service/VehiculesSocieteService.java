c042686c65653cc1a24a70db13a5d4c31f96dbbf
Audrey Martin <audrey.martin26@gmail.com>
2020-02-25 09:20:55 +0100

    creation vehicule de societe #29 modif statut par defaut

--- src/main/java/dev/service/VehiculesSocieteService.java
+++ src/main/java/dev/service/VehiculesSocieteService.java
@@ -9,9 +9,9 @@ import dev.repository.ReservationsSocieteRepo;
 import dev.repository.VehiculeSocieteRepo;
 import dev.controller.dto.VehiculesSocieteDTO;
 import dev.controller.dto.VehiculesSocieteFiltreDTO;
-import dev.controller.vm.ReservationCovoiturageVM;
 import dev.controller.vm.VehiculeSocieteVM;
 import dev.domain.ReservationsSociete;
+import dev.domain.Statut;
 import dev.domain.VehiculeSociete;
 import dev.exception.VehiculeNonTrouveException;
 import dev.exception.VehiculeTrouveException;
@@ -67,7 +67,14 @@ public class VehiculesSocieteService {
 		vehiculeNewPost.setMarque(vehiculeDTOPost.getMarque());
 		vehiculeNewPost.setModele(vehiculeDTOPost.getModele());
 		vehiculeNewPost.setCategorie(vehiculeDTOPost.getCategorie());
-		vehiculeNewPost.setStatut(vehiculeDTOPost.getStatut());
+		
+		if(vehiculeDTOPost.getStatut()!= null) {
+			vehiculeNewPost.setStatut(vehiculeDTOPost.getStatut());
+		}
+		else {
+			vehiculeNewPost.setStatut(Statut.EN_SERVICE);
+		}
+		
 		vehiculeNewPost.setUrlPhoto(vehiculeDTOPost.getUrlPhoto());
 		
 		this.vehiculesSocieteRepo.save(vehiculeNewPost);
